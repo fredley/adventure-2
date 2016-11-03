5f16800a9b063cf2efa5f08f1154e611037d388f
Jake Gillespie <jake.gillespie@onefinestay.com>
2016-11-03 20:22:38 +0000

    Add knowledge

---
+++ knowledge.py
@@ -0,0 +1,18 @@
+"""
+
+Information:
+
+    from knowledge import information
+
+    # Look up by index
+    piece = information[0]
+
+    # Access information via 'string' attribute.
+    piece['string']
+
+"""
+information = [
+    {'string': "If we remain, we won't join the euro."},
+    {'string': 'If we remain, we will maintain control of our borders.'},
+    {'string': 'If we remain, we will add restrictions to our welfare system to new EU migrants.'},
+]

09a61a0a094e60c5867c7ee5a5d5845f09ed4272
Tom White <tom.white@methodsdigital.co.uk>
2016-11-03 20:40:16 +0000

    Better map text

--- knowledge.py
+++ knowledge.py
@@ -13,11 +13,26 @@ Information:
 """
 
 map_text = {
-  'scotland': "You arrive at the Canon's Gait pub on the Royal Mile, the crowd is a bit rowdy...",
-  'wales': "You arrive in Wales feeling hungry, and feel like going out for a 'Chinese'.",
-  'west_country': "Arriving in the West Country by plane, you need a pint after a bumpy landing.",
-  'the_north': "You arrive in Manchester to a warm welcome by Morrisey",
-  'london': "You arrive in London, taking your large security entourage around with you, just in case.",
+  'scotland': {
+    'arrive': "You arrive at the Canon's Gait pub on the Royal Mile, the crowd is a bit rowdy...",
+    'name': "Scotland"
+  },
+  'wales': {
+    'arrive': "You arrive in Wales feeling hungry, and feel like going out for a 'Chinese'.",
+    'name': "Wales"
+  },
+  'west_country': {
+    'arrive': "Arriving in the West Country by plane, you need a pint after a bumpy landing.",
+    'name': "West Country"
+  },
+  'the_north': {
+    'arrive': "You arrive in Manchester to a warm welcome by Morrisey",
+    'name': "The North"
+  },
+  'london': {
+    'arrive': "You arrive in London, taking your large security entourage around with you, just in case.",
+    'name': "London"
+  }
 }
 
 information = [
--- map.py
+++ map.py
@@ -26,22 +26,28 @@ class UkMap(Map):
   def __init__(self, room='london'):
 
     self.rooms = {
-      'scotland': Room(map_text['scotland']),
-      'wales': Room(map_text['wales']),
-      'west_country': Room(map_text['west_country']),
-      'the_north': Room(map_text['the_north']),
-      'london': Room(map_text['london'])
+      'scotland': Room(map_text['scotland']['name']),
+      'wales': Room(map_text['wales']['name']),
+      'west_country': Room(map_text['west_country']['name']),
+      'the_north': Room(map_text['the_north']['name']),
+      'london': Room(map_text['london']['name'])
     }
 
     self.rooms['london'].west = self.rooms['west_country']
     self.rooms['london'].north = self.rooms['the_north']
+    self.rooms['london'].description = map_text['london']['description']
 
     self.rooms['west_country'].west = self.rooms['wales']
     self.rooms['west_country'].east = self.rooms['london']
+    self.rooms['west_country'].description = map_text['west_country']['description']
+
+    self.rooms['wales'].east = self.rooms['west_country']
 
     self.rooms['the_north'].north = self.rooms['scotland']
     self.rooms['the_north'].south = self.rooms['london']
+    self.rooms['the_north'].description = map_text['the_north']['description']
 
     self.rooms['scotland'].south = self.rooms['the_north']
+    self.rooms['scotland'].description = map_text['scotland']['description']
 
     self.current_room = self.rooms['london']
